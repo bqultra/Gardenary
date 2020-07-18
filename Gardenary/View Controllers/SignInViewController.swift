8095fbe7ec42db0e1298892f32b60b13c9cdcd07
Szymek <simondesignit@gmail.com>
2020-07-18 19:48:19 +0200

    added Show/Hide Password feature - Yay!

--- Gardenary/View Controllers/SignInViewController.swift
+++ Gardenary/View Controllers/SignInViewController.swift
@@ -12,21 +12,16 @@ class SignInViewController: UIViewController {
     
     //Buttons outlet
     @IBOutlet weak var fb_btn: UIButton!
-    
     @IBOutlet weak var google_btn: UIButton!
-    
     @IBOutlet weak var apple_btn: UIButton!
-    
     @IBOutlet weak var sign_up: UIButton!
     
     //Text fields
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
     
     //Form buttons
     @IBOutlet weak var forgotPassword: UIButton!
-    
     @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
@@ -34,26 +29,50 @@ class SignInViewController: UIViewController {
         
         //Hide keyboard tapping anywhere
         hideKeyboardWhenTappedAround()
-
-        // Do any additional setup after loading the view.
             
-        //Social buttons style
+            //Social buttons style
             fb_btn.layer.cornerRadius = 5
-            
             google_btn.layer.cornerRadius = 5
             google_btn.layer.borderWidth = 1
             google_btn.layer.borderColor = UIColor.black.cgColor
-            
             apple_btn.layer.cornerRadius = 5
             
             //Buttons inner elements margin
             fb_btn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 15)
-            
             google_btn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
-            
             apple_btn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+        
+        //Show/hide password - styling button inside UITextField
+        passwordFieldIcon.rightViewMode = .whileEditing
+        eyeButton.setImage(UIImage(named: "eye"), for: .normal)
+        let myColor = UIColor(red: 186/255, green: 186/255, blue: 186/255, alpha: 1.0) /* #bababa */
+        eyeButton.tintColor = myColor
+        //Padding
+        eyeButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
+        eyeButton.frame = CGRect(x: CGFloat(passwordFieldIcon.frame.size.width - 25), y: CGFloat(5), width: CGFloat(15), height: CGFloat(25))
+        
+        //Button tap action
+        eyeButton.addTarget(self, action: #selector(self.btnPasswordVisibility), for: .touchUpInside)
+        passwordFieldIcon.rightView = eyeButton
+        passwordFieldIcon.rightViewMode = .always
             
         }
+    
+    @IBOutlet weak var passwordFieldIcon: UITextField!
+    let eyeButton = UIButton(type: .custom)
+
+    //SHOW/HIDE PASSWORD *** Button tapped
+    @IBAction func btnPasswordVisibility(_ sender: Any) {
+        (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
+        if (sender as! UIButton).isSelected {
+            self.passwordFieldIcon.isSecureTextEntry = false
+            eyeButton.setImage(UIImage(named: "eye_closed"), for: .normal)
+        } else {
+            self.passwordFieldIcon.isSecureTextEntry = true
+            eyeButton.setImage(UIImage(named: "eye"), for: .normal)
+        }
+    }
+    
         
         //Light status bar
         override var preferredStatusBarStyle: UIStatusBarStyle {
--- Gardenary/View Controllers/SignUpViewController.swift
+++ Gardenary/View Controllers/SignUpViewController.swift
@@ -10,59 +10,86 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
-    //Back to Sign In button
-    @IBAction func signin_btn(_ sender: UIButton) {
-        _ = navigationController?.popViewController(animated: true)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Hide keyboard tapping anywhere
+        //Hide keyboard tapping anywhere - Source: Utilities
         hideKeyboardWhenTappedAround()
 
         // Do any additional setup after loading the view.
         hideInvalidErrors()
+        
+        //Show/hide password - styling button inside UITextField
+        passwordFieldIcon.rightViewMode = .whileEditing
+        eyeButton.setImage(UIImage(named: "eye"), for: .normal)
+        let myColor = UIColor(red: 186/255, green: 186/255, blue: 186/255, alpha: 1.0) /* #bababa */
+        eyeButton.tintColor = myColor
+        
+        //Padding
+        eyeButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
+        eyeButton.frame = CGRect(x: CGFloat(passwordFieldIcon.frame.size.width - 25), y: CGFloat(5), width: CGFloat(15), height: CGFloat(25))
+        
+        //Button tap action
+        eyeButton.addTarget(self, action: #selector(self.btnPasswordVisibility), for: .touchUpInside)
+        passwordFieldIcon.rightView = eyeButton
+        passwordFieldIcon.rightViewMode = .always
+    }
+    
+    
+    //Back to Sign In button
+    @IBAction func signin_btn(_ sender: UIButton) {
+        _ = navigationController?.popViewController(animated: true)
     }
     
     //White Status Bar
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    //Hide text fields errors
+    func hideInvalidErrors() {
+        firstNameValid.alpha = 0
+        emailValid.alpha = 0
+        passwordValid.alpha = 0
+    }
+    
+    //SHOW/HIDE PASSWORD *** Show or Hide password (Eye Icon)
+    
+    @IBOutlet weak var passwordFieldIcon: UITextField!
+    let eyeButton = UIButton(type: .custom)
+
+    //SHOW/HIDE PASSWORD *** Button tapped
+    @IBAction func btnPasswordVisibility(_ sender: Any) {
+        (sender as! UIButton).isSelected = !(sender as! UIButton).isSelected
+        if (sender as! UIButton).isSelected {
+            self.passwordFieldIcon.isSecureTextEntry = false
+            eyeButton.setImage(UIImage(named: "eye_closed"), for: .normal)
+        } else {
+            self.passwordFieldIcon.isSecureTextEntry = true
+            eyeButton.setImage(UIImage(named: "eye"), for: .normal)
+        }
+    }
  
     //Text fields
     @IBAction func firstNameTextField(_ sender: Any) {
     }
-    
     @IBAction func emailTextField(_ sender: Any) {
     }
-    
     @IBAction func passwordTextField(_ sender: Any) {
     }
     
     //Form buttons
     @IBAction func signUpButton(_ sender: Any) {
     }
-    
     @IBAction func termsOfUse(_ sender: Any) {
     }
-    
     @IBAction func privacyPolicy(_ sender: Any) {
     }
     
     //Form validation errors
-    
     @IBOutlet weak var firstNameValid: UILabel!
-    
     @IBOutlet weak var emailValid: UILabel!
-    
     @IBOutlet weak var passwordValid: UILabel!
-    
-    func hideInvalidErrors() {
-        firstNameValid.alpha = 0
-        emailValid.alpha = 0
-        passwordValid.alpha = 0
-    }
     
     //Validation function
     
